67c9cad3bb03dc6fa5391687c94237ad321495fa
Geovane Rocha <71407456+Georocha@users.noreply.github.com>
2022-03-17 21:58:07 -0300

    Add files via upload

---
+++ JavaScript/events/mouse/script.js
@@ -0,0 +1,24 @@
+function over() {
+    document.getElementById("colors").style.backgroundColor="red";
+    document.getElementById("log").innerHTML += "over the div / color: red<br>";
+}
+
+function forDown() {
+    document.getElementById("colors").style.backgroundColor="blue";
+    document.getElementById("log").innerHTML += "over the div / color: blue<br>";
+}
+
+function forUp() {
+    document.getElementById("colors").style.backgroundColor="green";
+    document.getElementById("log").innerHTML += "over the div / color: green<br>";
+}
+
+function moving() {
+    document.getElementById("colors").style.backgroundColor="yellow";
+    document.getElementById("log").innerHTML += "over the div / color: red<br>";
+}
+
+function goingOut() {
+    document.getElementById("colors").style.backgroundColor="black";
+    document.getElementById("log").innerHTML += "over the div / color: black<br>";
+}
